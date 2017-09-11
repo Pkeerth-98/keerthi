891bab7f47e827b5db607b82b6c21f0555c8442a
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2017-09-11 19:38:08 +0530

    Update and rename vowels to vowels.cpp

--- vowels.cpp
+++ vowels.cpp
@@ -5,19 +5,19 @@ cout<<"enter the character";
 cin>>s;
 switch(s)
 {
-case1:if(s=='a')
+case1:if(s=='a'||'A')
 cout<<"the char is vowel";
 break;
-case2:if(s=='e')
+case2:if(s=='e'||'E')
 cout<<"the char is vowel";
 break;
-case3:if(s=='i')
+case3:if(s=='i'||'I')
 cout<<"the char is vowel";
 break;
-case4:if(s=='o')
+case4:if(s=='o'||'O')
 cout<<"the char is vowel";
 break;
-case5:if(s=='u')
+case5:if(s=='u'||'U')
 cout<<"the char is vowel";
 break;
 default:
