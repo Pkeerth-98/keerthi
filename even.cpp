0ea4693dd101a8e7fb36d6e0f7fff4e179a00c8f
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2017-09-11 19:12:55 +0530

    Create even.cpp

---
+++ even.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+{
+int n,k;
+cout<<"enter the value to check odd or even";
+cin>>n;
+k=n%2;
+if(k==0)
+cout<<"number is even";
+else
+cout<<"number is odd";
+}
