5129866af2ea65b3bae7716a7612f46f091b883d
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2017-09-14 18:54:24 +0530

    Create consonant r vowel.cpp

---
+++ consonant r vowel.cpp
@@ -0,0 +1,26 @@
+#include<iostream.h>
+#include<conio.h>
+char s;
+cout<<"enter the character";
+cin>>s;
+switch(s)
+{
+case1:if(s=='a')
+cout<<"the char is vowel";
+break;
+case2:if(s=='e')
+cout<<"the char is vowel";
+break;
+case3:if(s=='i')
+cout<<"the char is vowel";
+break;
+case4:if(s=='o')
+cout<<"the char is vowel";
+break;
+case5:if(s=='u')
+cout<<"the char is vowel";
+break;
+default:
+cout<<"the char is consonant";
+}
+}
