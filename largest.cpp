7718dfe49995cc554f9ec95459b34a36a9473d70
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2017-09-15 18:53:20 +0530

    Create largest.cpp

---
+++ largest.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<conio.h>
+void main();
+{
+int a,b,c;
+cout<<"enter the numbers";
+cin>>a>>b>>C;
+if(a<b)
+{
+if(b<c)
+cout<<"the largest number is"<<c;
+else
+cout<<"the largestb  number is"<<b;
+}
+else if(a>b)
+{
+if(a>c)
+cout<<""the largest number is"<<a;
+else
+cout<<"the largestb  number is"<<b;
+}
+}
