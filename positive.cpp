e28fa242f97607c84430b77dc650f1dcc5dd33b0
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2017-09-16 19:05:58 +0530

    Create positive.cpp

---
+++ positive.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+float n;
+cout<<"enter the number";
+cin>>n;
+if(n>=0.00)
+cout<<" the num is positive";
+else
+cout<<"the num is negative";
+}
