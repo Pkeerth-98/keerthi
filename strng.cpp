75f4d2d8bb52e621108aada7378781aec61413b7
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2017-09-22 19:28:38 +0530

    Update strng.cpp

--- strng.cpp
+++ strng.cpp
@@ -2,10 +2,12 @@
 #include<conio.h>
 #include<string.h>
 void main()
-char a[100],c[100];
+{
+  char a[100],c[100];
 int i;
 cout<<"enter the string";
 for(i=0;i<100;i++)
 cin>>a[i];
 c=strrev(a);
 cout<<"reversed string"<<c;
+}
