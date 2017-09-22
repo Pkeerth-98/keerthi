31afe3e061ae6b53c64b8d6280fd49751021f4ba
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2017-09-22 19:27:42 +0530

    Create strng.cpp

---
+++ strng.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+#include<conio.h>
+#include<string.h>
+void main()
+char a[100],c[100];
+int i;
+cout<<"enter the string";
+for(i=0;i<100;i++)
+cin>>a[i];
+c=strrev(a);
+cout<<"reversed string"<<c;
