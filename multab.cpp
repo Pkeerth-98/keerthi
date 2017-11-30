a2b937f473afd4d208ab0e99e75ff3dd4e4c512e
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2017-11-30 19:39:04 +0530

    Create multab.cpp

---
+++ multab.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n,x,i;
+cout<<"enter the number of table";
+cin>>n;
+cout<<"multiplication table";
+for(i=1;i<=10;i++)
+{
+x=n*i;
+cout<<n*i=<<x;
+}
+}
