a13ab82bdf4e0eb57fe04189459b4b1400174ebb
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2017-11-30 18:41:49 +0530

    Create power.cpp

---
+++ power.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a,n,i;
+int s=1;
+cout<<"enter the base num";
+cin>>a;
+cout<<"enter the expo num";
+cin>>n;
+for(i=0;i<n;i++)
+{
+s=s*a
+}
+cout<<"result of power is"<<s;
+}
