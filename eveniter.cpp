9395153b2a81a44e5d770ee95e5c07703b3ba5b9
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2017-11-30 19:11:50 +0530

    Create eveniter.cpp

---
+++ eveniter.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n,x,y;
+cout<<"enter the intial interval value";
+cin>>x;
+couu<<"enter the final interval value";
+cin>>y;
+n=x;
+cout<<"the odd numbers are between intervals ";
+while((n=<y)&&(n=>x))
+{if(n%2==0)
+ {cout<<n;
+ }
+ n++;
+}
+}
